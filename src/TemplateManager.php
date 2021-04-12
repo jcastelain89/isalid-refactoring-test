890a8451194b5720a758beb547738d97956d7bd6
Jeremy CASTELAIN <jcastelain@wedigital.garden>
2021-04-12 22:42:52 +0200

    refactor: store in vars hard coded values :recycle:

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -21,43 +21,54 @@ class TemplateManager
 
         $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
+        $quoteAttributes = array(
+            'destination_link' => '[quote:destination_link]',
+            'destination_name' => '[quote:destination_name]',
+            'summary' => '[quote:summary]',
+            'summary_html' => '[quote:summary_html]',
+        );
+
+        $userAttributes = array(
+            'first_name' => '[user:first_name]'
+        );
+
         if ($quote)
         {
             $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
             $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-            if(strpos($text, '[quote:destination_link]') !== false){
+            if(strpos($text, $quoteAttributes['destination_link']) !== false){
                 $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
             }
 
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
+            $containsSummaryHtml = strpos($text, $quoteAttributes['summary_html']);
+            $containsSummary     = strpos($text, $quoteAttributes['summary']);
 
             if ($containsSummaryHtml !== false || $containsSummary !== false) {
                 if ($containsSummaryHtml !== false) {
                     $text = str_replace(
-                        '[quote:summary_html]',
+                        $quoteAttributes['summary_html'],
                         Quote::renderHtml($_quoteFromRepository),
                         $text
                     );
                 }
                 if ($containsSummary !== false) {
                     $text = str_replace(
-                        '[quote:summary]',
+                        $quoteAttributes['summary'],
                         Quote::renderText($_quoteFromRepository),
                         $text
                     );
                 }
             }
 
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+            (strpos($text, $quoteAttributes['destination_name']) !== false) and $text = str_replace($quoteAttributes['destination_name'],$destinationOfQuote->countryName,$text);
         }
 
         if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
+            $text = str_replace($quoteAttributes['destination_link'], $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
         else
-            $text = str_replace('[quote:destination_link]', '', $text);
+            $text = str_replace($quoteAttributes['destination_link'], '', $text);
 
         /*
          * USER
@@ -65,7 +76,7 @@ class TemplateManager
          */
         $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
         if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
+            (strpos($text, $userAttributes['first_name']) !== false) and $text = str_replace($userAttributes['first_name'], ucfirst(mb_strtolower($_user->firstname)), $text);
         }
 
         return $text;
